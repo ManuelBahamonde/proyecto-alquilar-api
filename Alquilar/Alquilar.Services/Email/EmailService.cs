3ae0f80818af8fc56c03e286cf33be534868d2f2
ManuelBahamonde <manuelbahamondedev@gmail.com>
2022-08-06 10:32:42 -0300

    Fixed emails

--- Alquilar/Alquilar.Services/Email/EmailService.cs
+++ Alquilar/Alquilar.Services/Email/EmailService.cs
@@ -26,20 +26,20 @@ namespace Alquilar.Services
         public void SendEmail(SendEmailRequest sendEmailRequest)
         {
             // Building Email
-            //var email = new MimeMessage
-            //{
-            //    Subject = ReplaceTagsFields(sendEmailRequest.Subject, sendEmailRequest.Tags),
-            //    Body = BuildHtmlMessageBody(sendEmailRequest.Body, sendEmailRequest.Tags)
-            //};
-            //email.From.Add(new MailboxAddress(_emailSettings.DisplayName, _emailSettings.Mail));
-            //email.To.Add(MailboxAddress.Parse(sendEmailRequest.To));
+            var email = new MimeMessage
+            {
+                Subject = ReplaceTagsFields(sendEmailRequest.Subject, sendEmailRequest.Tags),
+                Body = BuildHtmlMessageBody(sendEmailRequest.Body, sendEmailRequest.Tags)
+            };
+            email.From.Add(new MailboxAddress(_emailSettings.DisplayName, _emailSettings.Mail));
+            email.To.Add(MailboxAddress.Parse(sendEmailRequest.To));
 
-            //// Sending Email
-            //using var smtp = new SmtpClient();
-            //smtp.Connect(_emailSettings.Host, _emailSettings.Port);
-            //smtp.Authenticate(_emailSettings.Mail, _emailSettings.Password);
-            //smtp.Send(email);
-            //smtp.Disconnect(true);
+            // Sending Email
+            using var smtp = new SmtpClient();
+            smtp.Connect(_emailSettings.Host, _emailSettings.Port);
+            smtp.Authenticate(_emailSettings.Mail, _emailSettings.Password);
+            smtp.Send(email);
+            smtp.Disconnect(true);
         }
         #endregion
 
