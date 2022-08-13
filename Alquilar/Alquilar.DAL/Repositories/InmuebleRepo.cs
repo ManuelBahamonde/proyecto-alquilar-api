585796be95931643454ad1133af608bb2df3b4e7
ManuelBahamonde <manuelbahamondedev@gmail.com>
2022-08-13 14:43:19 -0300

    Made "Mis Propiedades" screen to show not available inmuebles

--- Alquilar/Alquilar.DAL/Repositories/InmuebleRepo.cs
+++ Alquilar/Alquilar.DAL/Repositories/InmuebleRepo.cs
@@ -23,7 +23,6 @@ namespace Alquilar.DAL
                 .Include(x => x.Localidad).ThenInclude(x => x.Provincia)
                 .Include(x => x.Usuario)
                 .Include(x => x.Imagenes)
-                .Where(x => !x.FechaHastaAlquilada.HasValue || DateTime.Now > x.FechaHastaAlquilada)
                 .AsQueryable();
 
             if (rq != null)
@@ -53,6 +52,8 @@ namespace Alquilar.DAL
 
                 if (rq.IdUsuario.HasValue)
                     query = query.Where(x => x.IdUsuario == rq.IdUsuario.Value);
+                else
+                    query = query.Where(x => !x.FechaHastaAlquilada.HasValue || DateTime.Now > x.FechaHastaAlquilada);
 
                 if (rq.IdTipoInmueble.HasValue)
                     query = query.Where(x => x.IdTipoInmueble == rq.IdTipoInmueble);
